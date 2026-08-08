9f4c21cb931ecadc44a04cbf16badc799a4de935
kashish <kgupta7827@gmail.com>
2026-08-09 01:41:54 +0530

    feat(target): add initial setup for target app with test  route

---
+++ apps/target/src/index.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+//tesing email
+const user = [{ email: "kgupta7827@gmail.com", name: "Kashish Gupta" }];
+
+//sample route with issues to trace the issue on the app
+
+app.post("/api/signup", async (req: Request, res: Response) => {
+  const { email, name, errorType } = req.body;
+
+  try {
+    console.log(`Sign up attempting ragistering for email : ${email}`);
+    if (errorType === "db_timout") {
+      console.log("Simulating latency spike");
+      await new Promise((resolve) => {
+        setTimeout(resolve, 3000);
+      });
+      throw new Error(
+        "DATBASE_ERROR: Connection pool exhausted while attempting to query users",
+      );
+    }
+
+    const existingUser = user.find((u) => u.email === email);
+    if (existingUser || errorType === "Duplicate") {
+      throw new Error(
+        "AUTHENTICATION_ERROR: User with email '${email}' already exists.",
+      );
+    }
+    // --- SUCCESS Case ---
+    user.push({ email, name });
+    return res.status(201).json({
+      message: "User registered successfully",
+      user: { email, name },
+    });
+  } catch (err: any) {
+    console.error(`❌ [SignUp Failed]: ${err.message}`);
+
+    return res.status(500).json({
+      error: "Signup failed",
+      details: err.message,
+    });
+  }
+});
