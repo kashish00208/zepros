ce0fa7266666f923e58b6c5fd6e728f6c4c1163d
kashish <kgupta7827@gmail.com>
2026-08-09 03:45:22 +0530

    i am so backkkk

--- apps/target/src/index.ts
+++ apps/target/src/index.ts
@@ -1,36 +1,55 @@
-import express from "express";
-import { Request, Response } from "express";
+import express from 'express'
+import { Request , Response } from 'express';
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { resourceFromAttributes } from "@opentelemetry/resources";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 
+const COLLECTOR_URL =
+  process.env.COLLECTOR_URL || "http://localhost:8080/v1/traces";
+
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "target-auth-service",
+  }),
+  traceExporter: new OTLPTraceExporter({
+    url: COLLECTOR_URL,
+  }),
+});
+
+sdk.start();
+console.log(` OpenTelemetry tracing sending to -> ${COLLECTOR_URL}`);
+
+// 2. Express route
 const app = express();
 app.use(express.json());
 
-//tesing email
-const user = [{ email: "test@gmail.com", name: "Kashish Gupta" }];
+const PORT = process.env.PORT || 4000;
 
-//sample route with issues to trace the issue on the app
+const user = [{ email: "test@gmail.com", name: "Kashish Gupta" }];
 
 app.post("/api/signup", async (req: Request, res: Response) => {
   const { email, name, errorType } = req.body;
 
   try {
-    console.log(`Sign up attempting ragistering for email : ${email}`);
-    if (errorType === "db_timout") {
-      console.log("Simulating latency spike");
-      await new Promise((resolve) => {
-        setTimeout(resolve, 3000);
-      });
+    console.log(`\n [SignUp] Attempting registration for: ${email}`);
+
+    if (errorType === "db_timeout") {
+      console.log("⏳ Simulating DB latency spike...");
+      await new Promise((resolve) => setTimeout(resolve, 3000));
       throw new Error(
-        "DATBASE_ERROR: Connection pool exhausted while attempting to query users",
+        "DATABASE_ERROR: Connection pool exhausted while attempting to query 'users' table."
       );
     }
 
     const existingUser = user.find((u) => u.email === email);
     if (existingUser || errorType === "Duplicate") {
       throw new Error(
-        "AUTHENTICATION_ERROR: User with email '${email}' already exists.",
+        `AUTHENTICATION_ERROR: User with email '${email}' already exists.`
       );
     }
-    // --- SUCCESS Case ---
+
+    // Success Case
     user.push({ email, name });
     return res.status(201).json({
       message: "User registered successfully",
@@ -44,4 +63,8 @@ app.post("/api/signup", async (req: Request, res: Response) => {
       details: err.message,
     });
   }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Target Auth App running on http://localhost:${PORT}`);
 });
